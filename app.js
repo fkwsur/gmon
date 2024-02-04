e1723f0b88d0755fbacfc2c63c9407161dd4c57b
Hyun-Ji <guswl543210@gmail.com>
2024-02-04 22:32:26 +0900

    2024-02-04_업데이트

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const app = require("./gmon");
 const Router = require("./routes")
 
+const aaa = () => {
+        console.log("찡니aa")
+}
+const bbb = () => {
+        console.log("찡니bb")
+}
+
 app.use("/api/v1/user", Router.userRouter)
 app.use("/api/v1/test", Router.testRouter)
+app.router("/api/v1/test", aaa)
+app.router("/api/v1/test2", bbb)
 
 app.listen(8081, async () => {
         console.log("server start")
--- gmon/gmon.js
+++ gmon/gmon.js
@@ -1,42 +1,60 @@
+const { checkPrimeSync } = require("crypto");
 const http = require("http");
+const { url } = require("inspector");
 const server = http.createServer();
 
 let set_use = [];
+let set_router = [];
+
 let req = {};
 let req_url = {};
+let onStart = false;
 
-server.on("request", async (data) => { 
-  for(const rows of set_use){
-    if(data.url.startsWith(rows.params1)){
-      req = data;
-      req_url = rows.params1;
-      let routes = rows.params2;
-      return routes();
+server.on("request", async (data) => {
+  onStart = true;
+  try {
+    req = data;
+
+    let data_router = set_router[data.url];
+    if (data_router != undefined) {
+      return data_router();
     }
+
+    for (const rows of set_use) {
+      if (data.url.startsWith(rows.url)) {
+        req_url = rows.url;
+        let routes = rows.func;
+        return routes();
+      }
+    }
+    return "주소 안맞음";
+  } catch (error) {
+    console.log(error);
   }
-  return "주소 안맞음"
 });
 
-const use = async (params1, params2) => {
-  set_use.push({params1: params1, params2 : params2})
-}
+const use = async (url, func) => {
+  set_use.push({ url: url, func: func });
+};
 
-const  router = async (router, func) => {
-    try {
-      if(req.url == req_url+router){
+const router = async (router, func) => {
+  try {
+    if (onStart == false) {
+      return set_router[router] = func;
+    } else if (req.url == req_url + router) {
         return func(req);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-}
+    }else return;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const  listen = async (port, func) => {
-    try {
-      return server.listen(port, func);
-    } catch (error) {
-      console.log(error);
-    }
-}
+const listen = async (port, func) => {
+  try {
+    return server.listen(port, func);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = {use,router,listen};
+module.exports = { use, router, listen };
