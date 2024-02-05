256d2003a5dbbbb612d4411c4f51f7214d667548
Hyun-Ji <guswl543210@gmail.com>
2024-02-05 21:34:30 +0900

    2024-02-05_업데이트

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const app = require("./gmon");
 const Router = require("./routes")
 
 const aaa = (req) => {
+        console.log(req.query)
         console.log(req.url + "찡니aa")
 }
 const bbb = (req) => {
+        console.log(req.body)
         console.log(req.url + "찡니bb")
 }
 const ccc = (req) => {
@@ -14,8 +16,10 @@ const ccc = (req) => {
 
 app.use("/api/v1/user", Router.userRouter)
 app.use("/api/v1/test", Router.testRouter)
+
 app.router.get("/api/v1/test", aaa)
 app.router.post("/api/v1/test2", bbb)
+
 app.get("/api/v1/test3", ccc)
 
 app.listen(8081, async () => {
